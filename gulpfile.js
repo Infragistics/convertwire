47b7b79b761df13ffdd33d108eaaf5aef690ee0b
Craig Shoemaker <craig@craigshoemaker.net>
2015-06-29 15:06:17 -0700

    added rename to gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var nodeDebug = require('gulp-node-debug');
 var docx2html = require('./modules/gulp-docx2html.js');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   return gulp.src('./spec/data/*.xml')
     .pipe(docx2html())
+    .pipe(rename(function(path){
+      path.extname = '.adoc';
+    }))
     .pipe(gulp.dest('./spec/data/done'));
 });
