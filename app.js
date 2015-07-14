d4d288e62223d8b19d91f2863020764f17379645
Craig Shoemaker <craig@craigshoemaker.net>
2015-07-14 15:24:12 -0700

    added stubs for gfm

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 // http://twolfson.com/2014-02-17-suggested-reading-for-writing-a-gulp-plugin
 
 var converter = require('./modules/html2AsciiDoc');
-var markup = '<div id="docX-root"><p>This is a <em>test</em> of the <a href="http://www.example.com"><strong>emergency</strong> broadcast</a> system</p></div>';
+//var markup = '<div id="docX-root"><p>This is a <em>test</em> of the <a href="http://www.example.com"><strong>emergency</strong> broadcast</a> system</p></div>';
 //var markup = '<div id="docX-root"><ul><li>1<ul><li>1.1</li></ul>1.a</li><li>2</li></ul></div>';
+var markup = '<table><thead><tr><th>Heading1</th></tr></thead><tbody><tr><td>cell1</td></tr></tbody></table>';
 var asciiDoc = converter.convert(markup);
 console.log(asciiDoc);
--- modules/html2AsciiDoc/index.js
+++ modules/html2AsciiDoc/index.js
@@ -1,11 +1,14 @@
 (function(module){
 	
 	var toMarkdown = require('to-markdown');
+	var gfm = require('./asciidoc-converters-gfm.js')
 	
 	var options = {
 		converters: require('./asciidoc-converters.js'),
 		gfm: true
 	}
+	
+	options.converters.concat(gfm);
 	
 	module.convert = function(htmlString){
 		return toMarkdown(htmlString, options);
