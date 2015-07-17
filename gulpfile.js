0e9d952d16056f3e4d50c0197051389ec774e9ce
Craig Shoemaker <craig@craigshoemaker.net>
2015-07-17 06:38:12 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,32 @@ var docx2html = require('./tasks/gulp-docx2html.js');
 var unmapper = require('./tasks/gulp-unmapper.js');
 var html2AsciiDoc = require('./tasks/gulp-html2AsciiDoc.js');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
 
-gulp.task('default', function() {
-  return gulp.src('./spec/data/*.xml')
+gulp.task('asciidoc', function() {
+  return gulp.src('./spec/data/src/*.xml')
     .pipe(docx2html())
     .pipe(unmapper())
     .pipe(html2AsciiDoc())
     .pipe(rename(function(path){
       path.extname = '.adoc';
     }))
-    .pipe(gulp.dest('./spec/data/done'));
+    .pipe(gulp.dest('./spec/data/dest'));
 });
 
 gulp.task('html', function() {
-  return gulp.src('./spec/data/*.xml')
+  return gulp.src('./spec/data/src/*.xml')
     .pipe(docx2html())
     .pipe(unmapper())
     .pipe(rename(function(path){
       path.extname = '.html';
     }))
-    .pipe(gulp.dest('./spec/data/done'));
+    .pipe(gulp.dest('./spec/data/dest'));
 });
+
+gulp.task('clean', function(){
+  return gulp.src('./spec/data/dest/**')
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean', 'asciidoc']);
