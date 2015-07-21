6992f363ff2029b40384f6671eb3f128be37e81e
Craig Shoemaker <craig@craigshoemaker.net>
2015-07-21 09:06:05 -0700

    updated greedy references

--- modules/html2AsciiDoc/index.js
+++ modules/html2AsciiDoc/index.js
@@ -11,13 +11,13 @@
 	var code = require('./converters-code.js').get();
 	var igStructure = require('./converters-ig-structure.js').get();
 	var structure = require('./converters-structure.js').get();
-	var greedy = require('./converters-greedy.js').get();
+	var general = require('./converters-general.js').get();
 	
 	addConverter(code);
 	addConverter(tables);
 	addConverter(igStructure);
 	addConverter(structure);
-	addConverter(greedy);
+	addConverter(general);
 	
 	module.convert = function(htmlString){
 		return toMarkdown(htmlString, {converters: converters});
