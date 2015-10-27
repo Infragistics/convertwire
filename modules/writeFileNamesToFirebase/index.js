051531baf631521cc88622566452d12751f1087f
Craig Shoemaker <craig@craigshoemaker.net>
2015-10-27 14:30:18 -0700

    update load to Firebase to read in product or control name

--- modules/writeFileNamesToFirebase/index.js
+++ modules/writeFileNamesToFirebase/index.js
@@ -1,12 +1,12 @@
 module = module.exports;
 
 var args = require('yargs')
-		.usage('Usage: node index.js $0 $1')
-		.demand(['username', 'email address'])
+		.usage('Usage: node index.js $0 $1 $2')
+		.demand(['username', 'password', 'productOrControlName'])
 		.argv;
 
 var relativePathToAsciiDocFiles = '../../spec/data/dest';
-
+var productOrControlName = args.productOrControlName
 var path = require('path');
 var Firebase = require('firebase');
 var fb = new Firebase('https://ig-topics.firebaseio.com/');
@@ -25,7 +25,7 @@ fb.authWithPassword({
 var extractFileName = require(path.resolve(__dirname, '../extractFileName'));
 var value = extractFileName.read(relativePathToAsciiDocFiles);
 
-var docs = fb.child('documents');
+var docs = fb.child('documents/' + productOrControlName);
 docs.set(value.documents, function(err, status){
 	console.log('File names loaded into Firebase.');
 });
