10c9348db6fe001bd737f150eeec4463ac15adba
Craig Shoemaker <craig@craigshoemaker.net>
2015-10-27 19:41:33 -0700

    update asciidoc gulp task to rename files with real file names

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,14 @@ var sourceFormatter = require('./tasks/gulp-sourceFormatter.js');
 var html2AsciiDoc = require('./tasks/gulp-html2AsciiDoc.js');
 var logger = require('./modules/logger');
 
-gulp.task('asciidoc', function(callback) {
+var remoteData = {};
+
+gulp.task('asciidoc-conversion', function(callback) {
   
   var onError = function(error){
     gutil.beep();
     logger.log(error);
   };
-  
   return gulp.src('./spec/data/src/*.xml')
     .pipe(plumber(onError))
     .pipe(docx2html())
@@ -26,6 +27,15 @@ gulp.task('asciidoc', function(callback) {
     .pipe(html2AsciiDoc())
     .pipe(rename(function(path){
       path.extname = '.adoc';
+      var isJP = path.basename.indexOf('.ja-JP') > -1;
+      var name = path.basename.replace(/\.ja-JP/i, '-ja-JP');
+      
+      if(remoteData[name]){
+        path.basename = remoteData[name];
+        path.basename += isJP ? '-ja-JP' : '';
+        
+        console.log(path.basename);
+      }
     }))
     .pipe(gulp.dest('./spec/data/dest'))
     .on('end', function(){
@@ -76,6 +86,41 @@ gulp.task('html-no-unmapper', function() {
 gulp.task('clean', function(){
   return gulp.src('./spec/data/dest/**')
     .pipe(clean());
+});
+
+gulp.task('asciidoc', function(){
+  
+  var args = require('yargs')
+              .usage('Usage: gulp asciidoc $0 $1 $2')
+		          .demand(['username', 'password', 'productOrControlName'])
+              .argv;
+              
+  var Firebase = require('firebase');
+  var fb = new Firebase('http://ig-topics.firebaseio.com');
+  
+  var docs = fb.child('documents/' + args.productOrControlName);
+  
+  fb.authWithPassword({
+    email: args.username,
+    password: args.password
+  }, function (error, authData) {
+    if (error) {
+      console.log('Login Failed!', error);
+    } else {
+      console.log('** Authenticated successfully **');
+      
+        console.log('Attempting to access Firebase data...');
+  
+        docs.once('value', function(snap){
+          remoteData = snap.val();
+          
+          console.log(Object.keys(remoteData).length + ' names loaded');
+          console.log('Starting AsciiDoc conversion...');
+          
+          return gulp.start('asciidoc-conversion');
+        });
+    }
+  });
 });
 
 gulp.task('default', ['clean', 'asciidoc']);
