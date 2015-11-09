58146e0273f9d8e38e8ff492937c474c7313a903
Craig Shoemaker <craig@craigshoemaker.net>
2015-11-09 15:37:47 -0800

    add new files

---
+++ spec/cleanup/cleanup.spec.js
@@ -0,0 +1,13 @@
+describe('cleanup', function(){
+	
+	var path = require('path');
+	var cleanup = require(path.resolve(__dirname, '../'));
+	
+	describe('clean', function(){
+		
+		it('replaces stray slashes in ordered lists', function(){
+			var src = '*1\.*';
+			var dest = 
+		});
+	});
+});
