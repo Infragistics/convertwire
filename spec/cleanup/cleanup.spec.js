883e611236344e43a6b195137d1510d7f409587b
Craig Shoemaker <craig@craigshoemaker.net>
2015-11-09 17:40:11 -0800

    update cleanup module with base implementation

--- spec/cleanup/cleanup.spec.js
+++ spec/cleanup/cleanup.spec.js
@@ -1,13 +1,29 @@
 describe('cleanup', function(){
 	
 	var path = require('path');
-	var cleanup = require(path.resolve(__dirname, '../'));
+	var cleanup = require(path.resolve(__dirname, '../../modules/cleanup'));
 	
 	describe('clean', function(){
 		
 		it('replaces stray slashes in ordered lists', function(){
 			var src = '*1\.*';
-			var dest = 
+			var dest = cleanup.clean(src);
+			var expected = '*1.*';
+			expect(dest).toEqual(expected);
+		});
+		
+		it('replaces sâ€¦ with ....', function(){
+			var src = 'Usersâ€¦';
+			var dest = cleanup.clean(src);
+			var expected = 'Users...';
+			expect(dest).toEqual(expected);
+		});
+		
+		it('replaces â„¢ with &trade;', function(){
+			var src = 'xamDataGridâ„¢';
+			var dest = cleanup.clean(src);
+			var expected = 'xamDataGrid&trade;';
+			expect(dest).toEqual(expected);
 		});
 	});
 });
