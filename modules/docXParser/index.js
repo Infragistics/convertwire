7d6a70d523aa0bd19b874e15800171c060f4b164
Craig Shoemaker <craig@craigshoemaker.net>
2015-11-19 10:59:31 -0800

    updated parser to replace known build flag GUIDs

--- modules/docXParser/index.js
+++ modules/docXParser/index.js
@@ -19,6 +19,16 @@
 				return "\"" + array.join("\",\"") + "\""; 
 			};
 			
+			var buildFlags = arrayToString(htmlDocument.buildFlags);
+			buildFlags = buildFlags
+								.replace('A33F8D9D-1A93-4A02-85E3-FC849DE1B8EA', 'WINFORMS')
+								.replace('34ADE70F-C190-412D-A2CE-25D1E1AE0FF8', 'WINRT')
+								.replace('{673B143B-6568-4204-99C0-4548E4AFEF3C}', 'WPF')
+								.replace('18DC7F35-922E-46A9-9127-9FA472AE43E2', 'ANDROID')
+								.replace('DROID_IN', 'ANDROID')
+								.replace('{A72AF817-CD06-4101-A8ED-A0E52FC4DD05}', 'SL')
+								.replace('27968E2C-EB4E-49F9-9A03-2FF58C6428F6', 'WINPHONE');
+			
 			var metaData = 
 				"<pre id='metadata'>\n" +
 "|metadata|\n" +
@@ -28,7 +38,7 @@
 "    \"controlName\": [" + arrayToString(htmlDocument.controlName) + "],\n" +
 "    \"tags\": [" + arrayToString(htmlDocument.tags) + "],\n" +
 "    \"guid\": \"" + htmlDocument.docXGuid.replace(/-/g, '`') + "\",\n" + // transform so later regex (that replaces guids) will not affect this value 
-"    \"buildFlags\": [" + arrayToString(htmlDocument.buildFlags) + "]\n" +
+"    \"buildFlags\": [" + buildFlags + "]\n" +
 "}\n" +
 "|metadata|\n" +
 "</pre>";
