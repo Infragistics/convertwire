f459c511a6bde28dd9faa8b5a042e6e74ad51914
Craig Shoemaker <craig@craigshoemaker.net>
2015-11-20 15:32:36 -0800

    update to report when there is no matching English topic to extract metadata

--- modules/docXParser/parser-japanese.js
+++ modules/docXParser/parser-japanese.js
@@ -1,56 +1,67 @@
 (function(module) {
 
-'use strict';
+	'use strict';
+		
+	const _ = require('lodash');
+	const fs = require('fs');
+	const path = require('path')
+	const utils = require('./utils.js')
+	const enParser = require('./parser-english.js');
+	const xml2js = require('xml2js');
+	const parser = new xml2js.Parser({
+		explicitArray: false
+	});
 	
-const _ = require('lodash');
-const fs = require('fs');
-const path = require('path')
-const utils = require('./utils.js')
-const enParser = require('./parser-english.js');
-const xml2js = require('xml2js');
-const parser = new xml2js.Parser({
-	explicitArray: false
-});
-
-module.parse = (xmlObj, filePath, callback) => {
-	let file, englishXML, htmlDocument, jpFileNameExpression;
-	
-	htmlDocument = utils.createHtmlDocument();
-	
-	jpFileNameExpression = /.ja-JP/;
-	
-	file = {
-		en: {
-			fileName: path.basename(filePath).replace(jpFileNameExpression, ''),
-			fullPath: filePath.replace(jpFileNameExpression, '')
-		},
-		jp: {
-			fileName: path.basename(filePath),
-			fullPath: filePath
+	module.parse = (xmlObj, filePath, callback) => {
+		let file, englishXML, htmlDocument, jpFileNameExpression, englishFileExists;
+		
+		htmlDocument = utils.createHtmlDocument();
+		
+		jpFileNameExpression = /.ja-JP/;
+		
+		file = {
+			en: {
+				fileName: path.basename(filePath).replace(jpFileNameExpression, ''),
+				fullPath: filePath.replace(jpFileNameExpression, '')
+			},
+			jp: {
+				fileName: path.basename(filePath),
+				fullPath: filePath
+			}
+		};
+		
+		englishFileExists = fs.existsSync(file.en.fullPath);
+		
+		if(englishFileExists) {
+			englishXML = fs.readFileSync(file.en.fullPath, 'utf8');
+			
+			parser.parseString(englishXML, (error, obj) => {
+				enParser.parse(obj, filePath, (parseError, englishHtmlDocument) => {
+					htmlDocument = englishHtmlDocument;
+					
+					let jpXML = fs.readFileSync(file.jp.fullPath, 'utf8');
+					
+					parser.parseString(jpXML, (jpParseError, jpObj) => {
+						
+						if(!_.isUndefined(jpObj.topic.title) && !_.isUndefined(jpObj.topic.title._)){
+							htmlDocument.title = jpObj.topic.title._.trim();
+						}
+						
+						if(!_.isUndefined(jpObj.topic.topicsection)){
+							htmlDocument.markup = jpObj.topic.topicsection._;
+						}
+						
+						callback(error, htmlDocument);
+					});
+				});
+			});
+		} else {
+			let error = {
+				message: 'Japanese topic does not have an English counterpart',
+				filePath: file.jp.fullPath,
+				tag: 'parse'
+			};
+			callback(error, null);
 		}
 	};
-	
-	englishXML = fs.readFileSync(file.en.fullPath, 'utf8');
-	
-	parser.parseString(englishXML, (error, obj) => {
-		enParser.parse(obj, filePath, (parseError, englishHtmlDocument) => {
-			htmlDocument = englishHtmlDocument;
-			
-			let jpXML = fs.readFileSync(file.jp.fullPath, 'utf8');
-			
-			parser.parseString(jpXML, (jpParseError, jpObj) => {
-				
-				if(!_.isUndefined(jpObj.topic.title) && !_.isUndefined(jpObj.topic.title._)){
-					htmlDocument.title = jpObj.topic.title._.trim();
-				}
-				
-				if(!_.isUndefined(jpObj.topic.topicsection)){
-					htmlDocument.markup = jpObj.topic.topicsection._;
-				}
-				
-				callback(error, htmlDocument);
-			});
-		});
-	});
-};
 }(module.exports));
