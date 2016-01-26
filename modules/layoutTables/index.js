e2895dc7e338ee7193d8004163a4b353d8d9e213
Craig Shoemaker <craig@craigshoemaker.net>
2016-01-26 12:48:07 -0800

    update layoutTables module to include single celled tables. closes #94

--- modules/layoutTables/index.js
+++ modules/layoutTables/index.js
@@ -8,40 +8,36 @@ if (isNodejsContext) {
     var module = {};
 }
 
-module.removeLayoutTables = (htmlString) => {
-    var result = '', $tables, isLayoutTable;
+var removeLayoutTables = (htmlString, options) => {
+    var $tables, isLayoutTable, idCounter, result;
     
-    if(isNodejsContext){
+    if (isNodejsContext) {
         $ = cheerio.load(htmlString);
     }
 
-    var $tables = $('#docX-root > table');
-
-    var idCounter = 0;
+    $tables = $(options.selector);
+    idCounter = 0;
 
     isLayoutTable = ($table) => {
-        var value, className, counters;
+        var value, className, counters = {};
 
         idCounter++;
-        className = `_${idCounter}-layout-table`;
+        className = `_${idCounter}-${options.label}`;
         $table.addClass(className);
-        $table.addClass('_layout-tables');
         $table.data('temp-id', className);
-
-        counters = {
-            tr: 0,
-            th: 0,
-            td: 0
-        }
 
         counters.tr = $(`.${className} > tbody > tr`).length;
         counters.th = $(`.${className} > tbody > tr > th`).length;
         counters.td = $(`.${className} > tbody > tr > td`).length;
-        
-        value = counters.tr === 1 &&
-                    counters.th === 0 &&
-                    counters.td === 2;
-                    
+
+        value = counters.tr === options.limits.tr &&
+                counters.th === options.limits.th &&
+                counters.td === options.limits.td;
+                
+        if(value){
+            $table.addClass(`_${options.label}s`);
+        }
+
         return value;
     };
 
@@ -49,10 +45,10 @@ module.removeLayoutTables = (htmlString) => {
         var $table = $(table);
         if (isLayoutTable($table)) {
             var markup = '', className, selector;
-            
-            className = $table.data('temp-id'); 
+
+            className = $table.data('temp-id');
             selector = `.${className} > tbody > tr > td`;
-            
+
             $(selector).each((cellIndex, cell) => {
                 markup += $(cell).html();
             });
@@ -61,11 +57,32 @@ module.removeLayoutTables = (htmlString) => {
         }
     });
 
-    $('._layout-tables').remove();
-
+    $(`._${options.label}s`).remove();
+    
     if (isNodejsContext) {
         result = $.html();
     }
-
+    
     return result;
+};
+
+module.removeLayoutTables = (htmlString) => {
+    
+    var twoColumnLayoutTableOptions = {
+        label: 'layout-table',
+        selector: '#docX-root > table',
+        limits: { tr: 1, th: 0, td: 2 }
+    };
+    
+    htmlString = removeLayoutTables(htmlString, twoColumnLayoutTableOptions);
+    
+    var oneCellLayoutTableOptions = {
+        label: 'single-cell-layout-table',
+        selector: 'table',
+        limits: { tr: 1, th: 0, td: 1 }
+    };
+    
+    htmlString = removeLayoutTables(htmlString, oneCellLayoutTableOptions);
+       
+    return htmlString;
 };
