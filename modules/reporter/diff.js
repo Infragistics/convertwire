b79eb6d8d0671e0c255f2efcbd3db487ba4420b3
Craig Shoemaker <craig@craigshoemaker.net>
2016-05-31 14:18:32 -0700

    fix diff order

--- modules/reporter/diff.js
+++ modules/reporter/diff.js
@@ -9,13 +9,18 @@ var diff = (path1, path2) => {
     var path1Files = fs.readdirSync(path1);
     var path2Files = fs.readdirSync(path2);
     var diffFiles = [];
+
+    console.log('detecting differences');
     
-    diffFiles = _.difference(path1Files, path2Files);
-    fs.writeFileSync(path1 + '\\diff.txt', diffFiles.join(os.EOL));
+    diffFiles = _.difference(path2Files, path1Files);
+    
+    console.log(`There are ${diffFiles.length} diff files.`);
+    
+    if(diffFiles.length > 0) {
+        fs.writeFileSync(path1 + '\\diff.txt', diffFiles.join(os.EOL));
+    }
+    
+    console.log('done');
 };
 
-console.log('detecting differences');
-
 diff(`C:\\Users\\cshoemaker\\Documents\\Code\\convertwire\\spec\\data\\dest`, `C:\\Users\\cshoemaker\\Documents\\Code\\convertwire\\spec\\data\\dest-rename`);
-
-console.log('done');
